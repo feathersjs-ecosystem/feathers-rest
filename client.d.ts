11b2bf5e35f2af383dbc65d148e0c671e3034bd3
abraaoalves <abraao.teodosio@gmail.com>
2017-01-17 04:42:02 -0300

    fix(definition): fix amd import

--- client.d.ts
+++ client.d.ts
@@ -1,30 +1,33 @@
-interface HandlerResult extends Function{
-  /**
-   * initialize service
-   */
-  () : void;
-  /**
-   * Transport Service
-   */
-  Service:any;
 
-  /**
-   * default Service
-   */
-  service:any;
+declare function rest(base: string): rest.Transport;
+
+declare namespace rest {
+  interface HandlerResult extends Function {
+    /**
+     * initialize service
+     */
+    (): void;
+    /**
+     * Transport Service
+     */
+    Service: any;
+
+    /**
+     * default Service
+     */
+    service: any;
+  }
+
+  interface Handler {
+    (connection, options?): () => HandlerResult;
+  }
+
+  interface Transport {
+    jquery: Handler;
+    superagent: Handler;
+    request: Handler;
+    fetch: Handler;
+  }
 }
-
-interface Handler {
-  (connection, options?) : () => HandlerResult;
-}
-
-interface Transport{
-  jquery:     Handler;
-  superagent: Handler;
-  request:    Handler;
-  fetch:      Handler;
-}
-
-declare function rest(base: string): Transport;
 
 export = rest;
